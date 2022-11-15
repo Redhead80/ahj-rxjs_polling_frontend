6fe5cb3b12bab5a171fd1a19312ccbd322db9d0e
Redhead80 <g.utkin80@yandex.ru>
2022-11-15 13:55:37 +0300

    commit

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@ import Widget from './Widget';
 
 const widget = new Widget(
   document.getElementById('root'),
-  'https://ahj-rxjs-polling-redhead80.herokuapp.com/messages/unread',
+  'https://ahj-11-1-rxjs-poling-sergius.herokuapp.com//messages/unread',
 );
 
 widget.subscribeToUpdate();
